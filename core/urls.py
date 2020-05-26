c45a08272b432f7a1501e03b18d2b1186823d976
Karen Barrera <barrerakari08@gmail.com>
2020-05-25 20:17:08 -0500

    Practica fusiona el Frontend

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.home, name="about"),
+    path('services/', views.home, name="services"),
+    path('contact/', views.home, name="contact"),
+    path('blog/', views.home, name="blog"),
+    path('sample/', views.home, name="sample"),
+]
